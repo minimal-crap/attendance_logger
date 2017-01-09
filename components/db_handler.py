d6c38d5ec9429a1854a72521161b911e0e3155a6
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-09 23:26:11 +0530

    modify db_handler to add method for table creation

---
+++ components/db_handler.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from conf.settings import db_settings as dbs
+from components.models import Employee
+
+database_url = "{0}://{1}:{2}@{3}:{4}/{5}".format(dbs['vendor'],
+                                                  dbs['user'],
+                                                  dbs['password'],
+                                                  dbs['host'],
+                                                  dbs['port'],
+                                                  dbs['db'])
+
+engine = create_engine(database_url)
+
+
+def create_employee_table():
+    try:
+        if not engine.dialect.has_table(engine,
+                                        Employee.__tablename__):
+            employee_obj = Employee()
+            employee_obj.metadata.create_all(engine)
+
+    except Exception as err:
+        print err.message
