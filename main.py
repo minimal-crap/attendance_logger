dcec4302c5a934bb886c6081534bfc6a46ced2b6
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-09 23:39:36 +0530

    modify main.py to add websocket handlers

---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+
+from tornado.ioloop import IOLoop
+from tornado.websocket import WebSocketHandler
+from tornado.web import Application
+
+
+active_clients = []
+
+
+def send_data_to_clients(message):
+    print "active cleints {}".format(len(active_clients))
+    for client in active_clients:
+        try:
+            client.write_message(message)
+        except Exception as err:
+            print "websocket err: {}".format(err.message)
+            active_clients.remove(client)
+
+
+class SocketOutputHandler(WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print "websocket opened"
+        if self not in active_clients:
+            active_clients.append(self)
+
+    def on_message(self, message):
+        self.write_message(json.loads(message))
+        print message
+
+    def on_close(self):
+        print "websocket closed!"
+        if self in active_clients:
+            active_clients.remove(self)
+
+
+
+
+
+class EmployeeInputHandler(Application):
+    def post(self, *args, **kwargs):
+        data = self.request.body
