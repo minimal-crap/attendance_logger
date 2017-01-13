e014fc6fac67ced048d273a3fe943f5ec9d30f95
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-13 23:18:48 +0530

    modify db_handler to fix db settings error

--- components/db_handler.py
+++ components/db_handler.py
@@ -3,14 +3,14 @@ import datetime
 
 from sqlalchemy import create_engine
 from conf.settings import db_settings as dbs
-from components.models import Employee
+from models import Employee
 
-database_url = "{0}://{1}:{2}@{3}:{4}/{5}".format(dbs['vendor'],
+database_url = "{0}://{1}:{2}@{3}:{4}/{5}".format(dbs['db_driver'],
                                                   dbs['user'],
                                                   dbs['password'],
                                                   dbs['host'],
                                                   dbs['port'],
-                                                  dbs['db'])
+                                                  dbs['database'])
 
 engine = create_engine(database_url)
 
@@ -31,11 +31,13 @@ def save_employee_data(data):
         conn = engine.connect()
         current_time = str(datetime.datetime.now())
         checkin_id = str(uuid.uuid4())
-        sql_string = "insert into {0} values({1}, {2}, {3}, {4}, {5})".format(checkin_id,
+        sql_string = "insert into {0} values('{1}', '{2}', '{3}', '{4}', '{5}')".format(Employee.__tablename__,
+                                                                              checkin_id,
                                                                               data['name'],
                                                                               data['designation'],
                                                                               data['department'],
                                                                               current_time)
+        conn.execute(sql_string)
+
     except Exception as err:
         print err.message
-
