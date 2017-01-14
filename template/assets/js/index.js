15b78762c7bf3e5e73c921ed341d039561e952d9
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-14 22:30:48 +0530

    modify index.js to add column fields

--- template/assets/js/index.js
+++ template/assets/js/index.js
@@ -1,16 +1,22 @@
-var web_socket_url = "ws://localhost:8002/get_attendance_data";
+var web_socket_url = "ws://localhost:8001/get_attendance_data";
 var data_set = [];
-var table_columns = [];
+var table_columns = [
+            {title:"id"},
+            {title:"name"},
+            {title:"designation"},
+            {title:"department"},
+            {title:"check-in time"}
+           ];
 $(document).ready(function(){
-    var ws = WebSocket(web_socket_url);
+    var ws = new WebSocket(web_socket_url);
+    console.log("testing");
 
     ws.onmessage = function(even){
         //logic to process incoming push messages
         //goes here
     };
 
-    $("#attendance-live-table").DataTable(
-        data: data_set;
-        columns: table_columns;
-    );
+    $("#attendance-live-table").DataTable({
+        data: data_set,
+        columns: table_columns});
 });
