f1b7b5f8dd7c8c2ec96b9b7f386cc7e3e2769851
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-15 23:28:05 +0530

    modify models.py to add in_out field in employee model

--- components/models.py
+++ components/models.py
@@ -10,6 +10,7 @@ class Employee(Base):
     id = Column(String(256), primary_key=True)
     name = Column(String(40))
     designation = Column(String(60))
+    in_out = Column(String(10))
     department = Column(String(60))
     checkin_timestamp = Column(DateTime)
 
